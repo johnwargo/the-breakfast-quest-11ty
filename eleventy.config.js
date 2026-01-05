9e885ca8b6aeb8d9f6ab2a87b675333e0231c634
johnwargo <john@johnwargo.com>
2026-01-05 17:54:07 -0500

    created folder structure and home page

---
+++ eleventy.config.js
@@ -0,0 +1,11 @@
+export default async function(eleventyConfig) {
+	return {
+		dir: {
+			input: 'src',
+			output: '_site',
+			includes: '_includes',
+			layouts: '_layouts',
+			data: '_data'
+		}
+	}
+};
