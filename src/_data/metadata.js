2d3b9d7e54728dead1a71ce814565c51b17197d6
johnwargo <john@johnwargo.com>
2026-03-10 20:26:22 -0400

    copied files from johnwargo.com

---
+++ src/_data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "The Breakfast Quest",
+	url: "https://thebreakfastquest.com",
+	language: "en",
+	description: "Reviews and articles about the breakfast places we've visited",
+	author: {
+		name: "John M. Wargo",
+		email: "&#106;&#111;&#104;&#110;&#064;&#106;&#111;&#104;&#110;&#119;&#097;&#114;&#103;&#111;&#046;&#099;&#111;&#109;",
+		url: "https://johnwargo.com"
+	}
+}
