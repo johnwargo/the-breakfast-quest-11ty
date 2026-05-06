50e349740fe9f3acf1f93c29b91f2e233b7c192b
johnwargo <john@johnwargo.com>
2026-05-05 20:10:38 -0400

    couple 'o things
    Broke listview records into includes
    WOrking on Search

--- src/assets/js/search-page.js
+++ src/assets/js/search-page.js
@@ -1,5 +1,4 @@
-
-function doSearch(evt) {
+async function doSearch(evt) {
 	// if triggered by the input field event, prevent default handling of the event.
 	if (evt) {
 		evt.preventDefault();
@@ -8,13 +7,18 @@ function doSearch(evt) {
 	const query = document.getElementById('queryInput').value.trim();
 	if (query.length > 0) {
 		console.log(`Executing search for: "${query}"`);
-		const client = algoliasearch('3GOZDBYV4K', 'df8b2d02bb72c7d1d5b92b1b8308d993');
-		const index = client.initIndex('site-search');
+		const { liteClient } = window["algoliasearch/lite"];
+		const client = liteClient("3GOZDBYV4K", "df8b2d02bb72c7d1d5b92b1b8308d993");
+		const response = await client.search([
+			{ indexName: "site-search", params: { query, hitsPerPage: 20 } }
+		]);
+		const hits = response.results[0].hits;
+		console.dir(hits);
 
-		// Perform a search
-		index.search(query).then(({ hits }) => {
-			console.dir(hits);
-		});
+		const results = document.querySelector("#searchResults");
+		results.innerHTML = hits
+			.map((hit) => `<article><h4><a href="${hit.url}">${hit.title || hit.name || hit.objectID}</a></h4></article>`)
+			.join("");
 	}
 };
 
